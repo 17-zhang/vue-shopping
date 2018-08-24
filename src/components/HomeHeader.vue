e92ceee221a233f38dcd00a4c6c6c15f694449fc
zhangyiqi0910 <1562070422@qq.com>
2018-08-24 15:10:30 +0800

    初始化

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="home-header">
+
+  </header>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    name: 'home-header'
+  }
+</script>
+
+<style scoped>
+
+</style>
